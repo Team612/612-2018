e17a28d8135343db6d14a3e28907970de5b0f827
Zach Perkins <thezperk@gmail.com>
2018-02-10 11:58:55 -0500

    Basic PID for lift

--- src/org/usfirst/frc/team612/commands/DefaultLift.java
+++ src/org/usfirst/frc/team612/commands/DefaultLift.java
@@ -1,20 +1,32 @@
 package org.usfirst.frc.team612.commands;
 
 import org.usfirst.frc.team612.robot.OI;
+import org.usfirst.frc.team612.subsystems.Lift;
 import org.usfirst.frc.team612.robot.Robot;
+import org.usfirst.frc.team612.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class DefaultLift extends Command {
-
-    public DefaultLift() {
+	WPI_TalonSRX lift_talon = new WPI_TalonSRX(RobotMap.talon_lift);
+    int prev_pos=0;
+    int height=0;
+    int RATE=50;
+    int DEADZONE=60;
+    public DefaultLift(int h) {
+    	height=h;
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.lift);
+    	
     }
 
     // Called just before this Command runs the first time
@@ -23,13 +35,12 @@ public class DefaultLift extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    Robot.lift.getTalon().set(OI.gunner.getY(Hand.kLeft));
+        	lift_talon.set(ControlMode.Position,height); 	
     }
-    
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
--- src/org/usfirst/frc/team612/subsystems/Lift.java
+++ src/org/usfirst/frc/team612/subsystems/Lift.java
@@ -3,7 +3,10 @@ package org.usfirst.frc.team612.subsystems;
 import org.usfirst.frc.team612.commands.DefaultLift;
 import org.usfirst.frc.team612.robot.RobotMap;
 
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -11,13 +14,20 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Lift extends Subsystem {
 	WPI_TalonSRX lift_talon = new WPI_TalonSRX(RobotMap.talon_lift);
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-    	setDefaultCommand(new DefaultLift());
+    	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	lift_talon.selectProfileSlot(0, 0);
+    	lift_talon.config_kF(0, 0.2, 5000); //The 5000 is the time out 
+    	lift_talon.config_kP(0, 0.2, 5000);//for setting  the configuration.(in milliseconds).
+    	lift_talon.config_kI(0, 0, 5000);
+    	lift_talon.config_kD(0, 0,5000);
+    	
     }
     public WPI_TalonSRX getTalon() {
     	return lift_talon;
