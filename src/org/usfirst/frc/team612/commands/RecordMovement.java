71d49de72c7e2bfd02ff3c9b07b60361fa7a0f87
Calvin Godfrey <calvinbbcard@gmail.co>
2018-02-17 13:45:47 -0500

    Changes.

--- src/org/usfirst/frc/team612/commands/RecordMovement.java
+++ src/org/usfirst/frc/team612/commands/RecordMovement.java
@@ -92,7 +92,7 @@ public class RecordMovement extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return end;
+        return false;
     }
 
     // Called once after isFinished returns true
